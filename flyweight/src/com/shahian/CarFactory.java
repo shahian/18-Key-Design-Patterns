5f2c3e53093822bda1bd8570212d41b397d8b001
h.shahian <h.shahian@haytech.ir>
2023-05-30 14:26:11 +0330

    add flyweight factory class

--- flyweight/src/com/shahian/CarFactory.java
+++ flyweight/src/com/shahian/CarFactory.java
@@ -1,8 +1,22 @@
 package com.shahian;
 
+import java.util.HashMap;
 import java.util.Map;
 
 // Flyweight factory
 public class CarFactory {
     private Map<String, Car> carCache;
+
+    public CarFactory() {
+        carCache = new HashMap<>();
+    }
+
+    public Car getCar(String brand) {
+        Car car = carCache.get(brand);
+        if (car == null) {
+            car = new CarImpl(brand);
+            carCache.put(brand, car);
+        }
+        return car;
+    }
 }
